ac27c6bc5060c88f0f59f3993937731d11f400a6
Igor Rezende <sr.igor.dev@gmail.com>
2025-02-03 17:45:04 -0300

    FIX: string araay parse

--- src/schema/_internal/validations/functions/string_array.ts
+++ src/schema/_internal/validations/functions/string_array.ts
@@ -5,13 +5,20 @@ import { e } from "../../handlers/messages";
 import { IValidationParams } from "../types";
 
 export default ({ name, key }: IValidationParams) => {
-  return z.array(
-    z.string({
-      required_error: e.required(name || key),
-      invalid_type_error: e.string(name || key),
-    }),
-    {
-      message: e.array(name || key),
-    }
+  return z.preprocess(
+    (value) => {
+      if (Array.isArray(value)) return value;
+      else if (typeof value === "string") return value?.split(",");
+      return value;
+    },
+    z.array(
+      z.string({
+        required_error: e.required(name || key),
+        invalid_type_error: e.string(name || key),
+      }),
+      {
+        message: e.array(name || key),
+      }
+    )
   );
 };
